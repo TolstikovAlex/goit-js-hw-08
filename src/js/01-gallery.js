3b32f4a6f3af274fcb2aec9d37b04c745da69b64
Alex Tolstikov <tolstikovalexander2@gmail.com>
2022-08-04 16:42:54 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,9 @@
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const createGalleryItemMarkup = ({ preview, description, original }) => {
   return `<div class="gallery__item">
   <a class="gallery__link" href=${original}>
@@ -20,11 +21,10 @@ const createGalleryItemMarkup = ({ preview, description, original }) => {
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
 galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
-т;
 
 console.log(galleryMarkup);
 
-let lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
